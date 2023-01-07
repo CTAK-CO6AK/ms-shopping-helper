578a37bb5c14d031112fc536e1e5c84caa104a18
CTAK-CO6AK <72935702+CTAK-CO6AK@users.noreply.github.com>
2023-01-07 14:36:14 +0400

    First release.

---
+++ tampermonkey.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Shopping Game Helper
+// @version      0.1
+// @description  You have to do first game by yourself
+// @author       CTAK_CO6AK
+// @match        https://www.msn.com/*/shopping
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=msn.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const { fetch: originalFetch } = window;
+    let game = 0;
+    let index = 0;
+
+    window.fetch = async (...args) => {
+        let [resource, config] = args;
+
+        let response = await originalFetch(resource, config);
+
+        const json = () => response.clone().json().then((data) => (data));
+
+        let resp = await json();
+
+        setTimeout(() => {
+            let bebra = JSON.parse(resp[0].data);
+            if(Array.isArray(bebra) && bebra.length == 4) {
+            let minPrice = parseFloat(bebra[0].priceInfo.price.slice(1), 10);
+            let minIndex = 1;
+            for(var i=0;i<3;i++){
+                let price = parseFloat(bebra[i].priceInfo.price.slice(1), 10);
+                if(price < minPrice){
+                    minPrice = price;
+                    minIndex = i+1;
+                }
+            }
+            document.title = `Game #${game}, press ${index}`;
+            index = minIndex;
+            game++;
+        }
+                         },1000);
+
+        response.json = json;
+        return response;
+    };
+
+})();
